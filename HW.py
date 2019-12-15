ccff668355fef7082da664119ef6767319fdf9e3
AnnaSnigur <manechka794@gmail.com>
2019-12-15 09:57:10 +0200

    hw

--- HW.py
+++ HW.py
@@ -2,13 +2,6 @@ from flask import Flask
 import requests
 from faker import Faker
 
-
-def faker():
-    fake = Faker("")
-    return {fake.name(): fake.email() for _ in range(10)}
-
-
-
 app = Flask("app")
 
 
@@ -20,9 +13,13 @@ def requirements():
     return content
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/faker")
+def faker():
+    fake = Faker("")
+    return {fake.name(): fake.email() for _ in range(10)}
 
+
+@app.route("/file")
 def file():
     f = open("hw.csv")
     content = f.read()
@@ -38,11 +35,19 @@ def file():
         height_sum += height
         weight = float(row.split(",")[2])
         weight_sum += weight
-    return (height_sum / number) * 2.54, (weight_sum / number) * 0.454
+        h = (height_sum / number) * 2.54
+        w = (weight_sum / number) * 0.454
+        list = (h,w)
+    return str(list)
 
 
+@app.route("/kosmos")
 def kosmos():
     kosmos = requests.get("http://api.open-notify.org/astros.json")
     p = kosmos.text
-    return p.count("name")
+    return str(p.count("name"))
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
 
