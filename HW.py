601c1cbc774c74a8ac80aabbde1f2ba0d7046d1f
AnnaSnigur <manechka794@gmail.com>
2019-12-15 10:20:31 +0200

    hw

--- HW.py
+++ HW.py
@@ -2,30 +2,27 @@ from flask import Flask
 import requests
 from faker import Faker
 
-
-def faker():
-    fake = Faker("")
-    return {fake.name(): fake.email() for _ in range(10)}
-
-
-
 app = Flask("app")
 
 
 @app.route("/")
 def requirements():
-    f = open("requirements.txt")
-    content = f.read()
+    with open("requirements.txt") as f:
+        content = f.read()
     f.close()
     return content
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/faker")
+def faker():
+    fake = Faker("")
+    return {fake.name(): fake.email() for _ in range(10)}
 
+
+@app.route("/file")
 def file():
-    f = open("hw.csv")
-    content = f.read()
+    with open("hw.csv") as f:
+        content = f.read()
     content = content.split("\n")[1:]
     number = 0
     height_sum = 0
@@ -38,11 +35,19 @@ def file():
         height_sum += height
         weight = float(row.split(",")[2])
         weight_sum += weight
-    return (height_sum / number) * 2.54, (weight_sum / number) * 0.454
+        h = (height_sum / number) * 2.54
+        w = (weight_sum / number) * 0.454
+        list = (h, w)
+    return str(list)
 
 
+@app.route("/kosmos")
 def kosmos():
     kosmos = requests.get("http://api.open-notify.org/astros.json")
     p = kosmos.text
-    return p.count("name")
+    return str(p.count("name"))
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
 
